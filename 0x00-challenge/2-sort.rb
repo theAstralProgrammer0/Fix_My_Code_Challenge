97f7fa4c0c02a23bc327ac85b39e025aecceac55
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-11 14:47:26 +0100

    Added corrected 2-sort.rb file

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -4,34 +4,27 @@
 #
 ###
 
-count = 0
-result = [0]
+result = []
 ARGV.each do |arg|
     # skip if not integer
-    if arg !~ /^-?[0-9]+$/
-        count += 1
-        next
+    next if arg !~ /^-?[0-9]+$/
+
     # convert to integer
     i_arg = arg.to_i
     
     # insert result at the right position
     is_inserted = false
     i = 0
-    l = result.size
-    if l == 0 && !is_inserted
-        result << i_arg
-        count += 1
-        next
-
-        while count < ARGV.size do
+    l = result.size 
+    while !is_inserted && i < l do
         if result[i] < i_arg
-            result.insert(i - 1, i_arg)
+            i += 1
         else
-            result << i_arg
-        i += 1
-        break
-        is_inserted = true
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
         end
+    end
     result << i_arg if !is_inserted
 end
 
